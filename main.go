dcfff583d16b300669d3ad85993ad3ac3ff70cf4
aaronangxz <angxze@gmail.com>
2022-12-28 02:13:15 +0800

    Added add_card endpoint mock

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/aaronangxz/RewardTracker/processors"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
@@ -10,5 +11,7 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.POST("add_card", processors.AddCard)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- processors/add_card.go
+++ processors/add_card.go
@@ -1,1 +1,31 @@
 package processors
+
+import (
+	"errors"
+	"github.com/aaronangxz/RewardTracker/rewards_tracker.pb/rewards_tracker"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func AddCard(c echo.Context) error {
+	req := new(rewards_tracker.AddCardRequest)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, req.CardDetails)
+	//return c.JSON(http.StatusOK, rewards_tracker.AddCardResponse{
+	//	CardId: proto.Int64(0),
+	//	ResponseMeta: &rewards_tracker.ResponseMeta{
+	//		ErrorCode:    proto.Int64(0),
+	//		ErrorMessage: proto.String("ok"),
+	//	},
+	//})
+}
+
+func verifyFields() error {
+	return errors.New("")
+}
+
+func isCardExists() bool {
+	return false
+}
